121a4b7ad666fee4f410acdf112faf85142f923b
bud <a123>
2019-06-27 12:58:45 +0800

    update Func Currency

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -435,6 +435,7 @@ public class HelloController {
             if (exrateTo == null){
                 this.replyText(replyToken,"錯誤");
             }
+            this.replyText(replyToken,exrateTo);
             BigDecimal exrate = new BigDecimal(exrateTo);
             if (exrate.toString() == null) {
                 this.replyText(replyToken,"錯誤\n"+exrate.toString()+"\n"+"USD"+currToExrate+"\n"+exrateMap.keySet());
