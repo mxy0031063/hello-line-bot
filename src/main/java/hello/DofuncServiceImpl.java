475b312f3662783b640d25f3bbdd901534a25ac8
bud <a123>
2019-07-13 10:47:17 +0800

    記帳功能 數據庫連接9 新增了 jfreechart 圖片輸出

--- src/main/java/hello/DofuncServiceImpl.java
+++ src/main/java/hello/DofuncServiceImpl.java
@@ -824,7 +824,8 @@ public class DofuncServiceImpl implements DofuncService {
             // 图例显示百分比:自定义方式， {0} 表示选项， {1} 表示数值， {2} 表示所占比例
             plot.setLegendLabelGenerator(new StandardPieSectionLabelGenerator("{0} ({2})"));
             String tableImagePath = tablename+".jpg";
-            File file = new File("../resources/static/tableAccountingImage/"+tableImagePath);
+
+            File file = new File(createUri("../resources/static/tableAccountingImage/"+tableImagePath));
             FileOutputStream fos=new FileOutputStream(file);
             ChartUtilities.writeChartAsJPEG(
                     fos,
