a6ad6c695755293bbdb217d6078057557e15f2ef
bud <a123>
2019-07-25 15:36:33 +0800

    redis 資源歸還設定

--- src/main/java/hello/utils/JedisFactory.java
+++ src/main/java/hello/utils/JedisFactory.java
@@ -8,7 +8,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class JedisFactory {
-    public static JedisPool getPool() throws URISyntaxException{
+
+    private static JedisPool jedisPool = null ;
+
+    private static void initPool() throws URISyntaxException{
         URI uri = new URI("redis://h:p54d90325594d2f7a5b26692271e6b5bedfa0badfe0adb20698d031e9fbeb0a08@ec2-34-227-251-50.compute-1.amazonaws.com:27179");
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMinEvictableIdleTimeMillis(1000*20);    // 逐出連接的最小空閒時間
@@ -18,11 +21,13 @@ public class JedisFactory {
         poolConfig.setTestOnBorrow(true); // 對拿到的連接檢驗
         poolConfig.setTestOnReturn(true);   // 對返回接連檢驗
         poolConfig.setTestWhileIdle(true);  // 對空閒的連接檢驗
-        return new JedisPool(poolConfig, uri);
-
+        jedisPool = new JedisPool(poolConfig, uri);
     }
 
     public static Jedis getJedis() throws URISyntaxException{
-        return getPool().getResource();
+        if (jedisPool == null){
+            initPool();
+        }
+        return jedisPool.getResource();
     }
 }
