4e9d718a1cd3ff4a9c0fef013efaa5fb395ec6b9
bud <a123>
2019-07-25 15:48:50 +0800

    redis 連接數設置

--- src/main/java/hello/utils/JedisFactory.java
+++ src/main/java/hello/utils/JedisFactory.java
@@ -15,9 +15,9 @@ public class JedisFactory {
         URI uri = new URI("redis://h:p54d90325594d2f7a5b26692271e6b5bedfa0badfe0adb20698d031e9fbeb0a08@ec2-34-227-251-50.compute-1.amazonaws.com:27179");
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMinEvictableIdleTimeMillis(1000*20);    // 逐出連接的最小空閒時間
-        poolConfig.setMaxTotal(10); // 最大連接數
-        poolConfig.setMaxIdle(5);   // 最大空閒數
-        poolConfig.setMinIdle(1);   // 最小空閒數
+        poolConfig.setMaxTotal(15); // 最大連接數
+        poolConfig.setMaxIdle(10);   // 最大空閒數
+        poolConfig.setMinIdle(3);   // 最小空閒數
         poolConfig.setTestOnBorrow(true); // 對拿到的連接檢驗
         poolConfig.setTestOnReturn(true);   // 對返回接連檢驗
         poolConfig.setTestWhileIdle(true);  // 對空閒的連接檢驗
