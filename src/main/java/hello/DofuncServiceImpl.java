219cc29954804fe25230ac5941de5cdf5bad435a
bud <a123>
2019-10-03 15:56:05 +0800

    推齊功能測試

--- src/main/java/hello/DofuncServiceImpl.java
+++ src/main/java/hello/DofuncServiceImpl.java
@@ -1294,7 +1294,8 @@ public class DofuncServiceImpl implements DofuncService {
             log.info("推齊功能的列表長度 : "+llen);
             if ( llen > 10 ){
                 // 刪除第一個元素
-                jedis.lpop(id);
+                String remove = jedis.lpop(id);
+                log.info("remove text : "+remove);
             }
         } catch (URISyntaxException e) {
             e.printStackTrace();
